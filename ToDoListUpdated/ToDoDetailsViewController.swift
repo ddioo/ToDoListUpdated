ae23ef2faf172cc8afbb2df8cf5bd201f7f1eeda
tetianaborysova <tetiana.borysova@empeek.io>
2022-07-06 19:42:05 +0200

    Added images storage and swipe deleting a single table view row

--- ToDoListUpdated/ToDoDetailsViewController.swift
+++ ToDoListUpdated/ToDoDetailsViewController.swift
@@ -11,18 +11,18 @@ class ToDoDetailsViewController: UIViewController {
     
     var toDoCD : ToDoCD? = nil
     
-
+    
     
     var toDo = ToDo()
-
+    
     @IBOutlet weak var toDoLabel: UILabel!
     
     @IBAction func doneTapped(_ sender: Any) {
-    
+        
         if let context = (UIApplication.shared.delegate as?
-        AppDelegate)?.persistentContainer.viewContext { if let toDo = toDoCD{
-        context.delete(toDo) }
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext() }
+                          AppDelegate)?.persistentContainer.viewContext { if let toDo = toDoCD{
+                              context.delete(toDo) }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext() }
         navigationController?.popViewController(animated: true)
     }
     
@@ -31,34 +31,34 @@ class ToDoDetailsViewController: UIViewController {
         super.viewDidLoad()
         
         if let toDo = toDoCD {
-        if toDo.priority == 1{
-            if let name = toDo.name{
-        toDoLabel.text = "🧐 Wichtig ❗️" + name }
+            if toDo.priority == 1{
+                if let name = toDo.name{
+                    toDoLabel.text = "🧐 Wichtig ❗️" + name }
+            }
+            else if toDo.priority == 2{
+                if let name = toDo.name{
+                    toDoLabel.text = " 🤓 Sehr Wichtig ❗️❗️  " + name
+                }
+            }else{
+                if let name = toDo.name{
+                    toDoLabel.text = name }
+                
+                
+                // Do any additional setup after loading the view.
+            }
+            
+            
+            //    @IBAction func doneTapped(_ sender: Any) {
+            //    }
+            /*
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
         }
-        else if toDo.priority == 2{
-            if let name = toDo.name{
-            toDoLabel.text = " 🤓 Sehr Wichtig ❗️❗️  " + name
-        }
-        }else{
-            if let name = toDo.name{
-        toDoLabel.text = name }
-
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-//    @IBAction func doneTapped(_ sender: Any) {
-//    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
     }}
--- ToDoListUpdated/ToDoTableViewController.swift
+++ ToDoListUpdated/ToDoTableViewController.swift
@@ -11,29 +11,8 @@ class ToDoTableViewController: UITableViewController {
     
     var toDoCDs: [ToDoCD] = []
     
-    
-   // var toDos:[ToDo] = []
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let toDo1 = ToDo()
-//        toDo1.name = "Write chapter one of iOS 14 book"
-//
-//        toDo1.priority = 1
-//        let toDo2 = ToDo()
-//
-//        toDo2.name = "Edit iOS 14 book"
-//        toDo2.priority = 2
-//
-//        toDos = [toDo1,toDo2] // fills up the array
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,29 +20,29 @@ class ToDoTableViewController: UITableViewController {
     }
     
     func getToDos(){
-    if let context = (UIApplication.shared.delegate as?
-    AppDelegate)?.persistentContainer.viewContext {
-    if let toDosFromCoreData = try? context.fetch(ToDoCD.fetchRequest()){
-    if let toDos = toDosFromCoreData as? [ToDoCD]{
-        toDoCDs = toDos
-        tableView.reloadData() }
+        if let context = (UIApplication.shared.delegate as?
+                          AppDelegate)?.persistentContainer.viewContext {
+            if let toDosFromCoreData = try? context.fetch(ToDoCD.fetchRequest()){
+                if let toDos = toDosFromCoreData as? [ToDoCD]{
+                    toDoCDs = toDos
+                    tableView.reloadData() }
+            }
+            
         }
-        
     }
-        }
-
+    
     // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
+    
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //        // #warning Incomplete implementation, return the number of sections
+    //        return 0
+    //    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return toDoCDs.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
@@ -73,18 +52,21 @@ class ToDoTableViewController: UITableViewController {
         
         if selectedToDo.priority == 1{
             if let name = selectedToDo.name{
-            cell.textLabel?.text = "🧐 Wichtig ❗️" + name
-        }
+                cell.textLabel?.text = "🧐 Wichtig ❗️" + name
+            }
         }
         else if selectedToDo.priority == 2{
             if let name = selectedToDo.name{
-          cell.textLabel?.text = "🤓 Sehr Wichtig ❗️❗️" + name
-        }
+                cell.textLabel?.text = "🤓 Sehr Wichtig ❗️❗️" + name
+            }
         }
         else{
             if let name = selectedToDo.name{
-          cell.textLabel?.text = name
+                cell.textLabel?.text = name
+            }
         }
+        if let data = selectedToDo.image{
+            cell.imageView?.image = UIImage(data:data)
         }
         return cell
     }
@@ -94,50 +76,55 @@ class ToDoTableViewController: UITableViewController {
         performSegue(withIdentifier: "moveToDetails", sender: selectedToDo)
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit
+                            editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let selectedToDo = toDoCDs[indexPath.row]
+                context.delete(selectedToDo)
+                (UIApplication.shared.delegate as?
+                 AppDelegate)?.saveContext()
+                getToDos()
+                
+            }
+        }
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let addToDoViewController = segue.destination as? AddToDoViewController{
             addToDoViewController.toDoTableViewController = self
-    }
+        }
         
         if let detailsToDoViewController = segue.destination as? ToDoDetailsViewController{
             if let selectedToDo = sender as? ToDoCD{
